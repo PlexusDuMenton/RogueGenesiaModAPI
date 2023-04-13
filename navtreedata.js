41479dbf4df0c7d387e247097baf171b0296e6c0
Ouadi <huard.ouadi@gmail.com>
2023-04-13 07:07:15 +0200

    Removed File list

--- navtreedata.js
+++ navtreedata.js
@@ -25,8 +25,8 @@
 var NAVTREE =
 [
   [ "RogueGenesiaModAPI", "index.html", [
-    [ "Packages", "namespaces.html", [
-      [ "Package List", "namespaces.html", "namespaces_dup" ]
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ]
     ] ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
